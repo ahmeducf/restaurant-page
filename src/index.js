4d4208ca1b21f244fd8dec338a470fc84725399f
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-04-24 21:33:31 +0200

    chore: Init display controller

--- src/index.js
+++ src/index.js
@@ -1,40 +1,5 @@
 import './index.css';
-import tabFirstLoad from './display/tab-first-load';
-import setActiveButton from './display/set-active-button';
-import Home from './components/home';
-import Menu from './components/menu';
-import Contact from './components/contact';
+import displayController from './display/controller';
 
-tabFirstLoad();
-
-const main = document.querySelector('main');
-const homeButton = document.getElementById('home-button');
-const menuButton = document.getElementById('menu-button');
-const contactButton = document.getElementById('contact-button');
-
-homeButton.addEventListener('click', () => {
-  if (homeButton.classList.contains('active')) {
-    return;
-  }
-  setActiveButton(homeButton);
-  main.innerHTML = '';
-  main.appendChild(Home());
-});
-
-menuButton.addEventListener('click', () => {
-  if (menuButton.classList.contains('active')) {
-    return;
-  }
-  setActiveButton(menuButton);
-  main.innerHTML = '';
-  main.appendChild(Menu());
-});
-
-contactButton.addEventListener('click', () => {
-  if (contactButton.classList.contains('active')) {
-    return;
-  }
-  setActiveButton(contactButton);
-  main.innerHTML = '';
-  main.appendChild(Contact());
-});
+displayController.loadDisplay();
+displayController.init();
