ddf2bfdf9111cdaf5cabac4bc38025149b8e504b
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-04-24 14:31:25 +0200

    chore: Add classes to elements

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,9 +3,12 @@ import Nav from './nav';
 
 const Header = () => {
   const header = document.createElement('header');
+
   const container = document.createElement('div');
+  container.classList.add('container');
 
   const restaurantName = document.createElement('h1');
+  restaurantName.classList.add('restaurant-name');
   restaurantName.textContent = 'Mozzafiato';
 
   const nav = Nav(['Home', 'Menu', 'Contact']);
--- src/components/header/nav/index.js
+++ src/components/header/nav/index.js
@@ -2,11 +2,16 @@ import './index.css';
 
 const Nav = (args) => {
   const nav = document.createElement('nav');
+
   const navItems = document.createElement('ul');
+  navItems.classList.add('nav-items');
 
   args.forEach((item) => {
     const navItem = document.createElement('li');
+
     const navButton = document.createElement('button');
+    navButton.classList.add('nav-item');
+
     navButton.textContent = item;
     navItem.appendChild(navButton);
     navItems.appendChild(navItem);
